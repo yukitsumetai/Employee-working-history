3ed6f0b71398637121aab586b0798a32da12bc76
Ekaterina <yukitsumetai@yandex.ru>
2021-12-04 18:05:56 +0300

    Test commit Why multi not suporrted seems like supported

--- Client/src/History.java
+++ Client/src/History.java
@@ -4,6 +4,8 @@ import java.util.List;
 public interface History extends Remote {
     List<HistoryObject>  getHistory(String surname) throws Exception;
     List<HistoryObject>  getHistory(int code);
+    //feature
+    HashMap<Integer> test = new HashMap();
     boolean login(String user, String password);
     boolean logout();
 }
